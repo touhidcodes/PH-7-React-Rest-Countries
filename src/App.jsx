023443518c389e7fda0795598ff8ad64e72fea3f
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-29 13:11:31 +0600

    load-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<Countries></Countries>
+		</div>
+	);
 }
 
-export default App
+function Countries() {
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		fetch("https://restcountries.com/v3.1/all")
+			.then((res) => res.json())
+			.then((data) => setCountries(data));
+	}, []);
+	return (
+		<div>
+			<h1>React Rest Countries</h1>
+			<p>Available Countries: {countries.length}</p>
+		</div>
+	);
+}
+
+export default App;
